9cffc3d0a85f6e7b3353ef1918344cd3e23d5542
tomaszbrauntsch <tomb@DESKTOP-7H64REG.>
2025-04-07 22:19:19 -0400

    initial-files

---
+++ v1/main-v1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <mpi.h>
+
+// https://zdimension.fr/everyone-gets-bidirectional-bfs-wrong/
+// https://www.thealgorists.com/Algo/TwoEndBFS
+
+int main(){
+    return 0;
+}
